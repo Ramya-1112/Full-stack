f9eacac8927dd78dc3f6c4d7a56ef38e51259adf
Ramya-1112 <rmayasri22@gmail.com>
2023-01-11 11:35:12 +0530

    commit my code

---
+++ js/html.js
@@ -0,0 +1,17 @@
+// function btn() {
+// var x=5;
+// var y=7;
+// var sum=x+y;
+// console.log(sum) 
+// console.log(typeof[x])
+
+// var x=5;
+// var y=7;
+// var sum=x+y;
+// document.write(sum);
+// var text= "hello thlis";
+// document.getElementById("demo").innerHTML= "hiiiiiiii";
+// }
+
+document.getElementById("demo").innerHTML = "hello everyone";
+
